0c22b5ea27019fa2f2ad4acad54b34b24dbe5fbb
TaikiHoshika <21060016ht@edu.fca.ac.jp>
2023-08-22 19:32:11 +0900

    [add][WIP] #2 再生コンポーネントを作成

---
+++ src/components/player/Player.jsx
@@ -0,0 +1,18 @@
+import PlayArrowRoundedIcon from '@mui/icons-material/PlayArrowRounded';
+import PauseRoundedIcon from '@mui/icons-material/PauseRounded';
+
+const Player = () => {
+    return (
+        <div>
+            <div>
+                <span></span>
+                <span></span>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    );
+}
+
+export default Player;
