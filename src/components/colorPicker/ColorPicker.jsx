09dcae4772c9a8b4d1a03dc7e6345c5f61431c2d
TaikiHoshika <21060016ht@edu.fca.ac.jp>
2023-08-28 19:46:28 +0900

    [modify] #2 フォルダ作成モーダルを作成

---
+++ src/components/colorPicker/ColorPicker.jsx
@@ -0,0 +1,23 @@
+
+import style from "./ColorPicker.module.css";
+
+const ColorPicker = ({colors, onChangeColor}) => {
+
+    const handlerChangeColor = (event) => {
+        onChangeColor(event.target.value);
+    }
+
+    return (
+        <div className={style.container}>
+            {colors.map((color) => (
+                <div className={style.colorPicker} style={{"background-color": color}}>
+                    <div className={style.button}>
+                        <input type="radio" name="color" value={color} onChange={handlerChangeColor}/>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default ColorPicker;
---
+++ src/components/modals/createFolder/CreateFolder.jsx
@@ -0,0 +1,71 @@
+import ColorPicker from "../../colorPicker/ColorPicker";
+import IconClose from "@mui/icons-material/CloseRounded";
+
+import style from "./CreateFolder.module.css"
+import { useState } from 'react';
+
+const CreateFolder = ({isShow, handlerToggleShow}) => {
+
+    const [name, setName] = useState("");
+    const handlerChangeText = (event) => {
+        setName(event.target.value);
+    }
+
+    const [color, setColor] = useState("#ffffff");
+    const handleChangeColor = (color) => {
+        setColor(color);
+        console.log(color);
+    }
+
+    const closeWindow = () => {
+        handlerToggleShow(!isShow);
+    }
+
+    const handlerCreateFolder = () => {
+        console.log(name);
+        console.log(color);
+        closeWindow();
+    }
+
+    const colors = [
+        "#E91E63",
+        "#FF9800",
+        "#FFD147",
+        "#CDDC39",
+        "#009688",
+        "#03A9F4",
+        "#3A52D7",
+        "#9C27B0",
+    ];
+
+    if (isShow) {
+        return (
+            <div className={style.background}>
+                <div className={style.container}>
+                    <div className={style.header}>
+                        <span>フォルダを作成</span>
+                        <button type="button" onClick={closeWindow}>
+                            <IconClose fontSize="large" />
+                        </button>
+                    </div>
+                    <div className={style.body}>
+                        <input
+                            type="text"
+                            placeholder="フォルダ名"
+                            onChange={handlerChangeText}
+                        />
+                        <ColorPicker
+                            colors={colors}
+                            onChangeColor={handleChangeColor}
+                        />
+                    </div>
+                    <button type="button" onClick={handlerCreateFolder} className={style.buttons}>
+                        作成
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default CreateFolder;
