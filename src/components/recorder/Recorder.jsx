4d07362520edf759483380f2ec3192d2027150f5
TaikiHoshika <21060016ht@edu.fca.ac.jp>
2023-08-30 12:23:15 +0900

    [modify][WIP] #1 録音画面を変更

---
+++ src/components/recorder/Recorder.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import Switch from "@mui/material/Switch";
+
+import IconName from "@mui/icons-material/CreateRounded";
+import IconColor from "@mui/icons-material/ColorLensRounded";
+import IconFolder from "@mui/icons-material/FolderRounded";
+import IronRecord from "@mui/icons-material/FiberManualRecordRounded";
+import IconStop from "@mui/icons-material/StopRounded";
+import IconLock from "@mui/icons-material/HttpsRounded";
+import IconNoLock from "@mui/icons-material/LockOpenRounded";
+
+import style from "./Recorder.module.css";
+
+const Recorder = () => {
+    const [isRecording, setIsRecording] = useState(false);
+    const handlerToggleRecording = () => {
+        setIsRecording((prev) => !prev);
+    }
+
+    const [isRecordButtonDisalbed, setIsRecordButtonDisalbed] = useState(false);
+    const handlerToggleRecordButton = () => {
+        setIsRecordButtonDisalbed((prev) => !prev);
+    }
+
+    return (
+        <div className={style.container}>
+            <span className={style.header}>録音</span>
+            <div className={style.description}>
+                <div className={`${style.title} ${isRecording ? style.isRecording : style.isNotRecording}`}>
+                    <input
+                        type="text"
+                        placeholder="タイトル"
+                        disabled={isRecording}
+                    />
+                    <IconName />
+                </div>
+                <div className={style.buttons}>
+                    <button type="button">
+                        <IconColor />
+                    </button>
+                    <button type="button">
+                        <IconFolder />
+                    </button>
+                </div>
+            </div>
+            <div className={style.recorder}>
+                <div className={style.switch}>
+                    {!isRecordButtonDisalbed ? (<IconNoLock />) : (<IconLock />)}
+                    <Switch
+                        onChange={handlerToggleRecordButton}
+                        sx={{color: "#fff"}}
+                    />
+                </div>
+                <button
+                    type="button"
+                    onClick={handlerToggleRecording}
+                    disabled={isRecordButtonDisalbed}
+                >
+                    {!isRecording ? (<IronRecord />) : (<IconStop />)}
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Recorder;
--- src/pages/record/Record.jsx
+++ src/pages/record/Record.jsx
@@ -1,27 +1,8 @@
-import { useState } from "react";
-import Switch from "@mui/material/Switch";
-
-import IconName from "@mui/icons-material/CreateRounded";
-import IconColor from "@mui/icons-material/ColorLensRounded";
-import IconFolder from "@mui/icons-material/FolderRounded";
-import IronRecord from "@mui/icons-material/FiberManualRecordRounded";
-import IconStop from "@mui/icons-material/StopRounded";
-import IconLock from "@mui/icons-material/HttpsRounded";
-import IconNoLock from "@mui/icons-material/LockOpenRounded";
+import Recorder from "../../components/recorder/Recorder";
 
 import style from "./Record.module.css";
 
 const Record = () => {
-    const [isRecording, setIsRecording] = useState(false);
-    const handlerToggleRecording = () => {
-        setIsRecording((prev) => !prev);
-    }
-
-    const [isRecordButtonDisalbed, setIsRecordButtonDisalbed] = useState(false);
-    const handlerToggleRecordButton = () => {
-        setIsRecordButtonDisalbed((prev) => !prev);
-    }
-
     return (
         <div className={style.container}>
             <div className={style.item}>
@@ -41,39 +22,8 @@ const Record = () => {
                 </div>
             </div>
 
-            <div className={`${style.item} ${style.record}`}>
-                <span className={style.header}>録音</span>
-                <div className={style.description}>
-                    <div className={style.title}>
-                        <IconName />
-                        <input
-                            type="text"
-                            placeholder="タイトル"
-                        />
-                    </div>
-                    <button type="button">
-                        <IconColor />
-                    </button>
-                    <button type="button">
-                        <IconFolder />
-                    </button>
-                </div>
-                <div className={style.recorder}>
-                    <div className={style.switch}>
-                        {!isRecordButtonDisalbed ? (<IconNoLock />) : (<IconLock />)}
-                        <Switch
-                            onChange={handlerToggleRecordButton}
-                            sx={{color: "#fff"}}
-                        />
-                    </div>
-                    <button
-                        type="button"
-                        onClick={handlerToggleRecording}
-                        disabled={isRecordButtonDisalbed}
-                    >
-                        {!isRecording ? (<IronRecord />) : (<IconStop />)}
-                    </button>
-                </div>
+            <div className={style.component}>
+                <Recorder />
             </div>
         </div>
     );
